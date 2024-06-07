0da93fbe72ad56a6136746b085d6e5053109f83f
Соловьёв Андрей Андреевич <Andrey.Solovyev@evraz.com>
2024-06-07 09:02:11 +0300

    Third commit

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ Simple is better than complex.
 Complex is better than complicated.
 Flat is better than nested.
 Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
 """
